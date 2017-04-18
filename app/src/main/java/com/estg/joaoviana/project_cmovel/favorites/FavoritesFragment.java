163e51c9581c27e243548450799c9ab019ea0d40
pjota <cheapinfor@mail.com>
2017-04-18 03:26:02 +0100

    ORDER favorites

--- app/src/main/java/com/estg/joaoviana/project_cmovel/favorites/FavoritesFragment.java
+++ app/src/main/java/com/estg/joaoviana/project_cmovel/favorites/FavoritesFragment.java
@@ -8,6 +8,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.MenuInflater;
@@ -18,8 +19,10 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,11 +31,14 @@ import com.estg.joaoviana.project_cmovel.Database.Contrato;
 import com.estg.joaoviana.project_cmovel.Database.DB;
 import com.estg.joaoviana.project_cmovel.R;
 
+import org.w3c.dom.Text;
+
 
 public class FavoritesFragment extends Fragment {
 
     View rootView;
-
+    Spinner sp;
+    ImageView img_arrow;
     DB mDbHelper;
     SQLiteDatabase db;
     Cursor c;
@@ -75,6 +81,8 @@ public class FavoritesFragment extends Fragment {
         btn_update.setVisibility(View.INVISIBLE);
         btn_cancel = (Button)getActivity().findViewById(R.id.btn_cancel);
         btn_cancel.setVisibility(View.INVISIBLE);
+        img_arrow = (ImageView)getActivity().findViewById(R.id.img_arrow);
+        img_arrow.setTag("up");
 
         btn_update.setOnClickListener(new View.OnClickListener()
         {
@@ -95,10 +103,53 @@ public class FavoritesFragment extends Fragment {
                 cancelButton();
             }
         });
+        img_arrow.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                String tag = (String)img_arrow.getTag();
+                String val = sp.getSelectedItem().toString();
+                String[] parts = val.split("-");
+                String part1 = parts[0];
+                if(tag.equals("up")){
+                    img_arrow.setTag("down");
+                    img_arrow.setImageDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.icon_arrow_down));
+                    preencheLista(part1,"DESC");
+
+                }else{
+                    img_arrow.setTag("up");
+                    img_arrow.setImageDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.icon_arrow_up));
+                    preencheLista(part1,"ASC");
+                }
+            }
+        });
+
+        sp = (Spinner) getActivity().findViewById(R.id.spinner);
+        sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                String val = parentView.getItemAtPosition(position).toString();
+                String[] parts = val.split("-");
+                String part1 = parts[0];
+                String tag = (String)img_arrow.getTag();
+                if(tag.equals("up")) {
+                    preencheLista(part1,"ASC");
+                }else{
+                    preencheLista(part1,"DESC");
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+
+        });
 
         listFavorites = (ListView) getActivity().findViewById(R.id.listFavorites);
         registerForContextMenu(listFavorites);
-        preencheLista();
+        preencheLista("1","ASC");
     }
 
     @Override
@@ -142,22 +193,22 @@ public class FavoritesFragment extends Fragment {
 
     }
 
-    private void preencheLista() {
+    private void preencheLista(String order_table,String order_by) {
+        String ord_table="";
+        if(order_table.equals("1")){
+            ord_table = Contrato.Place.COLUMN_NAME;
+        }else if(order_table.equals("2")){
+            ord_table = Contrato.Place.COLUMN_VICINITY;
+        }else if(order_table.equals("3")){
+            ord_table = Contrato.Place.COLUMN_DESCRIPTION;
+        }
         c = db.rawQuery("select "+Contrato.Place._ID+", "+Contrato.Place.COLUMN_ID+", "+
                 Contrato.Place.COLUMN_NAME+", "+ Contrato.Place.COLUMN_ICON+", "+
                 Contrato.Place.COLUMN_LATITUDE+", "+ Contrato.Place.COLUMN_LONGITUDE+", "+
                 Contrato.Place.COLUMN_DESCRIPTION+", "+ Contrato.Place.COLUMN_VICINITY+
-                " FROM " + Contrato.Place.TABLE_NAME, null);
-/*
-        String[] adapterCols = new String[]{Contrato.Place._ID,Contrato.Place.COLUMN_ID};
-        int[] adapterRowViews = new int[]{android.R.id.text1,android.R.id.text2};
-        adapter = new SimpleCursorAdapter(
-                getActivity(),
-                android.R.layout.simple_list_item_2,
-                c,
-                adapterCols,
-                adapterRowViews,
-                SimpleCursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);*/
+                " FROM " + Contrato.Place.TABLE_NAME+" ORDER BY "+ ord_table+" "+order_by
+                , null);
+
         mAdapter = new MyCursorAdapter(getContext(),c);
 
 
@@ -192,7 +243,17 @@ public class FavoritesFragment extends Fragment {
         }
     }
     private void refresh() {
-        preencheLista();
+        String tag = (String)img_arrow.getTag();
+        String val = sp.getSelectedItem().toString();
+        String[] parts = val.split("-");
+        String part1 = parts[0];
+
+        if(tag.equals("up")) {
+            preencheLista(part1,"ASC");
+        }else{
+            preencheLista(part1,"DESC");
+        }
+
         mAdapter.swapCursor(c);
     }
 
